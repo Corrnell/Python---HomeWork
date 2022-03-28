4f8df57fcd7f079f3154824a293f43bbc35f7385
Corrnell <102417729+Corrnell@users.noreply.github.com>
2022-03-28 18:03:26 +0300

    Add files via upload

---
+++ Product name.py
@@ -0,0 +1,28 @@
+productName = "\"Pizza\""
+productPrice = 10.00
+deliveryCost = 25.00
+
+print(productName,":",productPrice,"lei")
+
+answer = input("Do you wanna buy? ")
+
+if answer.lower() == "yes" or "y":
+    orderQuantity = int(input("How many: "))
+    orderCost = orderQuantity * productPrice
+
+    print("------------------")
+    print("ORDER INFO")
+    print(productName, "x",orderQuantity, "=", orderCost)
+
+    answer2 = input("Do you want any delivery: ")
+
+
+    if answer2.lower() == "yes" or "y":
+        orderCost += deliveryCost
+        print("Order cost including delivery will be", orderCost)
+    elif answer2.lower() != "yes" or "y":
+        print("Too bad")
+        print("Order cost without including delivery will be", orderCost)
+elif answer.lower() != "yes" or "y":
+    print("Too bad")
+    quit()
