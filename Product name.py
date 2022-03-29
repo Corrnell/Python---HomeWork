b1159bbf16bd879beb1b6fbb5fe15af31bfa8511
Corrnell <102417729+Corrnell@users.noreply.github.com>
2022-03-29 16:21:40 +0300

    Update Product name.py

--- Product name.py
+++ Product name.py
@@ -25,4 +25,32 @@ if answer.lower() == "yes" or "y":
         print("Order cost without including delivery will be", orderCost)
 elif answer.lower() != "yes" or "y":
     print("Too bad")
+    quit()productName = "\"Pizza\""
+productPrice = 10.00
+deliveryCost = 25.00
+
+print(productName,":",productPrice,"lei")
+
+answer = input("Do you wanna buy? ")
+
+if answer.lower() == "yes" or answer.lower() == "y":
+    orderQuantity = int(input("How many: "))
+    orderCost = orderQuantity * productPrice
+
+    print("------------------")
+    print("ORDER INFO")
+    print(productName, "x",orderQuantity, "=", orderCost)
+
+    answer2 = input("Do you want any delivery: ")
+
+
+    if answer2.lower() == "yes" or answer2.lower() == "y":
+        orderCost += deliveryCost
+        print("Order cost including delivery will be", orderCost)
+    elif answer2.lower() != "yes" or answer2.lower() != "y":
+        print("Too bad :(")
+        print("Order cost without including delivery will be", orderCost)
+elif answer.lower() != "yes" or answer.lower() != "y":
+    print("Too bad :(")
     quit()
+
