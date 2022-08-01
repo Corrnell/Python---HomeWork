f9ed55b52c404cdb9cb5d8dbec20a5a554c5cf23
Corrnell <102417729+Corrnell@users.noreply.github.com>
2022-08-01 21:01:09 +0300

    Update 1_Neuron.py

--- 1_Neuron.py
+++ 1_Neuron.py
@@ -8,16 +8,21 @@ y = []
 xt0 = [0,   5, 10, 15, 20, 25, 27, 30, 35, 40] # < temp
 xt1 = [10, 20, 30, 35, 40, 45, 50, 50, 65, 70] # < hum
 yt =  [0,   0,  0,  0,  0,  1,  1,  1,  1,  1] 
+W0 = 0
+W1 = 0
 # learning rate
 lr = 0.001 
 epochs = 0
 accuracy0 = [0 for a in range(len(xt0))]
 accuracy1 = [0 for b in range(len(xt1))]
-def safe_div(e):
-    if e == 0:
+def safe_div(x):
+    if x == 0:
         return 0
-    return 1 / e
+    else:
+        return 1 /x
+
 while epochs < 1:
+
     for i in range(len(xt0)): 
         for j in range(len(xt1)):
             Xt0 = xt0[i]
@@ -28,8 +33,7 @@ while epochs < 1:
 
             B = 0 
 
-            W0 = 0
-            W1 = 0
+
 
             _Y = int(Xt0 * W0 + Xt1 * W1 + B)
 
@@ -41,36 +45,28 @@ while epochs < 1:
             dW1 = lr * e * Xt1
 
             W0 += dW0
-            W1 += dW1 
+            W1 += dW1
             print(f"{i:3}:{Xt0:8}{Xt1:8}{Yp:8}{Yt:8}{e:8}{dW0:8.3}{W0:8.3}{dW1:8.3}{W1:8.3}")
 
             accuracy0[i] = safe_div(e)
             accuracy1[i] = safe_div(e)
     epochs += 1
 print("Epochs  Delta Weight  Weight")
-print(f"{epochs:6} {dW0:12} {W0:6}")
+print(f"{epochs:6} {dW0:12.4}   {W0:6.4}")
 
 print("Training DONE!")
 
-accuracy_avg = (sum(accuracy0) + sum(accuracy1)) / (len(accuracy0) + len(accuracy1))
-
+accuracy_avg = (sum(accuracy0) + sum(accuracy1)) / (len(accuracy0)+ len(accuracy1))
+print(accuracy0)
+print(accuracy1)
 
 print(f"ACCURACY: {accuracy_avg*100}%")
 
-for i in range(len(x0)):
-    for j in range(len(x1)):
-
-        Xt0 = x0[i]
-        Xt1 = x1[j] 
-
-        _Y = int(Xt0 * W0 + Xt1 * W1 + B) 
-
-        Yp = 1 if _Y >= 1 else 0 
-
-        y.append(Yp) 
-
-print(f"{str(x0):3}:{str(Xt0):8}\n\n{str(x1):3}:{str(Xt1):8}")
-
+# for j in range(len(x1)):
+#     for i in range(len(x0)):
+#         Xt0 = x0[i]
+#         Xt1 = x1[j]
+#         _Y = int(Xt0 * W0 + Xt1 * W1 + B)
+#         Yp = 1 if _Y >= 1 else 0 
+#         print(f"{j:3}:{Xt0:8}{Xt1:8}{Yp:8}{Yt:8}") 
 print("Testing DONE!")
-
-    # plt.plot(x,y)
